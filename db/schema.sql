c4001f83b904d4ef4b9e67717199a108609c905d
Ella Thompson <fang98t@gmail.com>
2020-11-29 18:38:29 -0500

    scheme role table

--- db/schema.sql
+++ db/schema.sql
@@ -10,7 +10,10 @@ CREATE TABLE department (
 
 --create roles table
 CREATE TABLE role (
-
+    id INT auto_increment PRIMARY KEY NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(8,2) NOT NULL,
+    department_id INT NOT NULL
 );
 
 --create employees table
