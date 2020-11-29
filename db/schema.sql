659d6c1ae8a674f1014a0cf3679f9b4d61631deb
Ella Thompson <fang98t@gmail.com>
2020-11-29 18:31:06 -0500

    schema department table

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,17 @@ DROP DATABASE IF EXISTS employees_DB;
 CREATE DATABASE employees_DB;
 
 --create departments table
+CREATE TABLE department (
+    id INT auto_increment PRIMARY KEY NOT NULL,
+    name VARCHAR(30) NOT NULL
+);
 
 --create roles table
+CREATE TABLE role (
+
+);
 
 --create employees table
+CREATE TABLE employee (
+
+);
