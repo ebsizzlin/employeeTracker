e79619afa2693d2a898289e3883e76441a3592c6
Ella Thompson <fang98t@gmail.com>
2020-11-29 20:16:33 -0500

    department values added

--- db/seed.sql
+++ db/seed.sql
@@ -1,17 +1,18 @@
 USE employees_DB;
 
 --department seed
-INSERT INTO department
-    (name)
+INSERT INTO department (name)
 VALUES
-
+    ("Production"),
+    ("Supervisory"),
+    ("Story Artist"),
+    ("Asset Artist"),
+    ("Shot Artist");
 
 --role seed
-INSERT INTO roles
-    (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES
 
 --employee seed
-INSERT INTO employee
-    (name)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
