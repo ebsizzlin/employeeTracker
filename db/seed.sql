6070869d634ae5059de9402aa8db39b414a58795
Ella Thompson <fang98t@gmail.com>
2020-11-29 20:26:01 -0500

    added commas

--- db/seed.sql
+++ db/seed.sql
@@ -12,25 +12,25 @@ VALUES
 --role seed
 INSERT INTO roles (title, salary, department_id)
 VALUES
-    ("Line Producer", 90, 1)
-    ("PA", 80, 1)
-    ("Runner", 80, 1)
-    ("VFX", 100, 2)
-    ("CG", 100, 2)
-    ("Lead", 80, 2)
-    ("Concept", 70, 3)
-    ("Storyboard", 70, 3)
-    ("Previs", 70, 3)
-    ("Modeller", 60, 4)
-    ("Texture", 60, 4)
-    ("Rigging", 60, 4)
-    ("Cloth", 60, 4)
-    ("Animator", 60, 5)
-    ("Matte", 60, 5)
-    ("Lighting", 70, 5)
-    ("Rotoscope", 70, 5)
-    ("FX", 70, 5)
-    ("Compositor", 90, 5)
+    ("Line Producer", 90, 1),
+    ("PA", 80, 1),
+    ("Runner", 80, 1),
+    ("VFX", 100, 2),
+    ("CG", 100, 2),
+    ("Lead", 80, 2),
+    ("Concept", 70, 3),
+    ("Storyboard", 70, 3),
+    ("Previs", 70, 3),
+    ("Modeller", 60, 4),
+    ("Texture", 60, 4),
+    ("Rigging", 60, 4),
+    ("Cloth", 60, 4),
+    ("Animator", 60, 5),
+    ("Matte", 60, 5),
+    ("Lighting", 70, 5),
+    ("Rotoscope", 70, 5),
+    ("FX", 70, 5),
+    ("Compositor", 90, 5);
 
 --employee seed
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
