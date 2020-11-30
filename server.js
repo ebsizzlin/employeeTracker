bbd75f5b4fbc6673764941a243af586c1c729dfe
Ella Thompson <fang98t@gmail.com>
2020-11-29 21:31:58 -0500

    throwing error and creating promptUser function

--- server.js
+++ server.js
@@ -2,3 +2,26 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var cTable = require("console.table");
+
+//creating connection info
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Ella98th',
+    database: 'employees_DB'
+});
+
+//connecting to mysql server & database
+connection.connect((err) => {
+    if (err) throw err;
+    promptUser();
+})
+
+//function to prompt user
+promptUser = () => {
+    inquirer
+        .prompt({
+            
+        })
+}
