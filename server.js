729cb89cd1ff78cbdd167eb9e635c522974069fa
Ella Thompson <fang98t@gmail.com>
2020-11-29 23:09:25 -0500

    attempt at addDepartment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var cTable = require("console.table");
+const { join } = require("path");
 
 //creating connection info
 const connection = mysql.createConnection({
@@ -23,7 +24,7 @@ promptUser = () => {
     inquirer
         .prompt({
             type: 'list',
-            name: 'action',
+            name: 'choice',
             message: 'Pick one:',
             choices: [
                 "View departments",
@@ -76,8 +77,8 @@ anotherChoice = () =>   {
             else {
                 connection.end();
                 console.log('Success!');
-            }
-        })
+            };
+        });
 }
 
 //vew departments
@@ -112,17 +113,72 @@ viewEmployees = () => {
 
 //add departments
 addDepartment = () => {
-
+    inquirer
+        .prompt({
+            type: 'input',
+            name: 'department',
+            message: 'Name of the department?'
+        })
+        .then((answer) => {
+            var query = "INSERT INTO department (name) VALUES ( ? )";
+            connection.query(query, (err, res) => {
+                console.log('Department added!')
+                if (err) throw (err);
+                console.table(res);
+                anotherChoice();
+            });
+        });
 }
 
 //add roles
 addRole = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'Name of the role?'
+            },
+            {
+                type: 'input',
+                name: 'salary',
+                message: 'Salary of new role?'
+            },
+            {
+                type: 'input',
+                name: 'department_id',
+                message: 'Department ID for role?',
+            }
+        ])
+        .then((res) => {
+            var title = res.title;
+            var salary = res.salary;
+            var department_id = res.department_id;
 
+            var query = 'INSERT INTO role (title, salary, department_id) VALUE("${title}", "${salary}", "${department_id}")';
+            connection.query(query, (err, res) => {
+                if (err) throw err;
+                console.table(res);
+                anotherChoice();
+            })
+        });
 }
 
 //add employees
 addEmployee = () => {
-
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'first_name',
+                message: "Employee's first name?"
+            },
+            {
+                type: 'input',
+                name: 'last_name',
+                message: "Employee's last name?"
+            },
+        ])
 }
 
 //update employees
